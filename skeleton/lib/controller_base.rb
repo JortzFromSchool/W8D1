1d25931892a5a22ea0849fc4833e0c8c419c8ff2
JortzFromSchool <johnnydortiz@gmail.com>
2021-09-27 14:21:15 -0400

    Phase 1 complete

--- skeleton/lib/controller_base.rb
+++ skeleton/lib/controller_base.rb
@@ -8,20 +8,31 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    @res.status = 302
+    @res["Location"] = url
+    nil
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "double render error" if already_built_response?
+    @res.write(content)
+    @res.content_type = content_type
+    @already_built_response = true
+    nil
   end
 
   # use ERB and binding to evaluate templates
